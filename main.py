9c46d08026bc2216d70ca998d8e57d44aaa328b1
PHFernandes9 <paulohenriquefernandes3008@gmail.com>
2025-04-01 20:05:35 -0300

    Função para gerar graficos

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-
-import pandas as pd
 import numpy as np
-
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 def learquivo():
     
     df = pd.read_csv('dados\CONSUMO.csv')
     return df
 
+# def grafico(df):
+
+
+def plota_graficos(df):
+
+    Norte = df[df['Regiao']=='Norte'] 
+    y =Norte['Consumo']
+    x =Norte['Data']
+
+    plt.bar(x,y)
+    plt.show()
+    print(Norte)
+
 
 
 if __name__=="__main__":
 
-    print(learquivo()['Consumo'])
-
+    dados = (learquivo())
+    plota_graficos(dados)
 
